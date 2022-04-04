149668cf771cc7ef94d23ddf67c65aa4c565beaa
Deepak Gupta <dgupta966@gmail.com>
2022-04-04 17:06:50 +0530

    webpack added

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const obj = {
+    name : "deepak",
+    city : "new delhi",
+    age :25
+};
+
+console.log(obj);
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+module.exports = {
+    mode: "production",
+    devServer : {
+        static: {
+            directory: path.join(__dirname, "dist")
+          },
+          compress: true,
+          port: 3010,
+        }
+};
